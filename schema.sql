9dcab14ea4757ad081ad5f735438b31842900272
PaddyHQ <patrickjholmquist@gmail.com>
2022-01-19 12:47:03 -0800

    finished retirement info

--- schema.sql
+++ schema.sql
@@ -32,14 +32,14 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-CREATE TABLE Dept_Emp (
-  dept_no VARCHAR NOT NULL,
-     emp_no INT NOT NULL,
-     from_date DATE NOT NULL,
-     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+CREATE TABLE Dept_Emp (emp_no INT NOT NULL, 
+					   dept_no VARCHAR(4) NOT NULL, 
+					   from_date DATE NOT NULL, 
+					   to_date DATE NOT NULL, 
+					   FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
+					   FOREIGN KEY (dept_no) REFERENCES departments (dept_no), 
+					   PRIMARY KEY (emp_no, dept_no)
+					   );
 );
 CREATE TABLE Titles (
   emp_no INT NOT NULL,
